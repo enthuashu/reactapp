637ac3f253cfe7bb049e6ee1e5de10bdf1c4848e
Ashutosh Mishra <69295774+enthuashu@users.noreply.github.com>
2022-08-13 12:30:44 +0530

    new

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import React from "react";
-import Navbar from "./Components/navbar"; // this returns navbar html code
-import Center from "./Components/center"; // this returns center html code
-import Footer from "./Components/footer"; // this returns footer html code
+// import Navbar from "./Components/navbar"; // this returns navbar html code
+// import Center from "./Components/center"; // this returns center html code
+// import Footer from "./Components/footer"; // this returns footer html code
 
 const App = () => {
   return (
     <>
-      <h1>Hello from the Parent Component</h1>
-      <Navbar />
-      <Center />
-      <Footer />
+      <h1 className="myname" id="name">
+        Hello from the Parent Component
+      </h1>
     </>
   );
 };
--- src/Components/footer.js
+++ src/Components/footer.js
@@ -1,9 +1,16 @@
 import React from "react";
 
+// html have two types of tags,
+// 1.paired tags which are enclosed e.g. <h1>hey ashu</h1>, <div></div>,<p></p> etc
+// 2.single tags which don't need enclosing e.g. <img src="photo"> <input type="text" required=true >, <a href="heh" >
+//
+//
 const footer = () => {
   return (
     <>
       <h1>Hello this is footer</h1>
+      <img src="testimage" />
+      <br />
     </>
   );
 };
